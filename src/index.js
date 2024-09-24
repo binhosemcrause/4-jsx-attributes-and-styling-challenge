87773652ab42536766e2788eab15a3faf5ca991e
binhosemcrause <binhosemcrause@gmail.com>
2024-09-24 18:23:19 +1000

    upload project

---
+++ src/index.js
@@ -0,0 +1,9 @@
+//Create a react app from scratch.
+//It should display 2 images.
+//In your CSS put the images in the same size
+
+// If you're running this locally in VS Code use the commands:
+// npm install
+// to install the node modules and
+// npm start
+// to launch your react project in your browser
